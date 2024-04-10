b0817a6d8e194b729be147c19546c2dee3c7bce9
Aakarshit13 <116973255+Aakarshit13@users.noreply.github.com>
2024-04-10 11:57:48 +0530

    added dotenv package

--- e_shop/e_shop/settings/prod.py
+++ e_shop/e_shop/settings/prod.py
@@ -1,8 +1,21 @@
-import os
 from .common import *
+from os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('NAME'),
+        'HOST': os.getenv('HOST'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD')
+    }
+}
+
+SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ["15.207.14.156"]
