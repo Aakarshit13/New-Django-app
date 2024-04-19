23167e00370709bdf4da3b6d417ffd57edb4a7a3
Aakarshit13 <116973255+Aakarshit13@users.noreply.github.com>
2024-04-19 09:41:37 +0530

    module settings changed from dev to prod

--- e_shop/e_shop/celery.py
+++ e_shop/e_shop/celery.py
@@ -1,7 +1,7 @@
 import os 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_shop.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_shop.settings.prod')
 
 celery = Celery('e_shop')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
